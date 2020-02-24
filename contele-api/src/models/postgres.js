fb674fdc4c90d181be5b9bd4b4eb00137a5925d5
Lucas Lima <lucass.limaa@outlook.com.br>
2020-02-24 18:43:09 -0300

    Update Readme

--- contele-api/src/models/postgres.js
+++ contele-api/src/models/postgres.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const db = new Pool({
-  user: "postgres",
-  password: "password",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   host: "localhost",
   port: 5432,
   database: "contele"
